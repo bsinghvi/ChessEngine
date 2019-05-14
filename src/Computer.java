f045b594a1f491d30b5589912c2b040b6c9a4d24
Bhaskar Singhvi <31942378+bsinghvi@users.noreply.github.com>
2019-05-14 14:42:31 -0400

    Create Computer.java
    Beginning of coding the computer player

---
+++ src/Computer.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+
+public class Computer {
+	HashMap<String, Integer> pieceRank; 
+	public Computer() {
+		pieceRank = new HashMap<String, Integer>(); 
+	}
+	/**
+	 * Master method for calculating best move based on difficulty
+	 * @param difficulty - 0 for Easy, 1 for Medium, and 2 for hard
+	 * @return int[] with four values, 2 for initial position, 2 for ending position
+	 */
+	public int[] caculateBestMove(int difficulty) {
+		
+		return null;
+	}
+}
